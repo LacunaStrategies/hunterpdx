df45c08b43b2825b2fc0aebbe0197e81a87795a0
Lacuna Strategies <lacunastrategies@gmail.com>
2023-03-31 17:30:55 -0700

    Updated Navigation
    Increased desktop breakpoint to 992px.
    Added desktop to nav styling.
    Added React-Icons dependency.
    Tweaked global :root CSS variables.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,27 @@
+// ** NextJS Imports
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+// ** React Imports
 import { useState } from 'react'
+
+// ** React Icons Imports
+import { RiMenu3Fill } from 'react-icons/ri'
+import { FiFacebook, FiInstagram, FiLinkedin } from 'react-icons/fi'
+
+// ** Styles
 import styles from './Header.module.scss'
 
 const navItems = [
     {
         label: 'Home',
-        path: '#home',
+        path: '/#home',
         title: 'Navigate to top of page',
     },
     {
         label: 'About',
-        path: '#about',
+        path: '/#about',
         title: 'Navigate to About Us section',
     },
     {
@@ -36,26 +46,31 @@ const socialItems = [
         label: 'Facebook',
         path: '#',
         title: 'Like Dustin Hunter on Facebook',
-        icon: '',
+        icon: <FiFacebook aria-hidden="true" />,
     },
     {
         label: 'Instagram',
         path: '#',
         title: 'Follow Dustin Hunter on Instagram',
-        icon: '',
+        icon: <FiInstagram aria-hidden="true" />,
     },
     {
         label: 'LinkedIn',
         path: '#',
         title: 'Connect with Dustin Hunter on LinkedIn',
-        icon: '',
+        icon: <FiLinkedin aria-hidden="true" />,
     },
 ]
 
 const Header = () => {
 
+    // State Variables
     const [showMenu, setShowMenu] = useState(false)
 
+    // Hooks
+    const router = useRouter()
+
+    // Functions
     const toggleNav = (): void => {
         setShowMenu(!showMenu)
     }
@@ -66,35 +81,38 @@ const Header = () => {
                 <div className={styles.navWrapper}>
                     {/* Header Logo Image */}
                     <div>
-                        <Image src="/assets/images/dustin-hunter-circle-profile-small.png" alt="Small circle profile picture of Dustin Hunter" height={70} width={70} />
+                        <Link href="/" title="Navigate to top of page">
+                            <Image src="/assets/images/dustin-hunter-circle-profile-small.png" alt="Small circle profile picture of Dustin Hunter" height={70} width={70} />
+                        </Link>
                     </div>
 
                     {/* Mobile Nav Toggle */}
                     <div className={styles.navToggle}>
                         <button onClick={() => toggleNav()} aria-label="Toggle mobile navigation menu">
-                            |||
+                            <RiMenu3Fill aria-hidden="true" />
                         </button>
                     </div>
 
                     {/* Nav Overlay - Mobile Only */}
                     <div
-                        onClick={() => toggleNav()}
+                        onClick={() => setShowMenu(false)}
                         className={`${styles.mobileOverlay} ${showMenu ? styles.open : ''}`}
                     ></div>
-                    
+
                     {/* Nav Menu */}
                     <nav className={showMenu ? `${styles.open}` : ''}>
                         <div className={styles.mobileNavTop}>
                             {/* Nav Top - Mobile Only */}
-                            <Image src="/assets/images/dustin-hunter-circle-profile-small.png" alt="Small circle profile picture of Dustin Hunter" height={70} width={70} />
-
+                            <Link onClick={() => setShowMenu(false)} href="/" title="Navigate to top of page">
+                                <Image src="/assets/images/dustin-hunter-circle-profile-small.png" alt="Small circle profile picture of Dustin Hunter" height={70} width={70} />
+                            </Link>
                             {/* Close Mobile Nav Button */}
                             <button
                                 className={styles.closeNav}
-                                onClick={() => toggleNav()} aria-label="Toggle mobile navigation menu">
+                                onClick={() => setShowMenu(false)} aria-label="Close mobile navigation menu">
                                 X
                             </button>
-                            <p>Skill and Passion Combined</p>
+                            <p>Skill And Passion Combined</p>
                         </div>
 
                         {/* END Nav Top */}
@@ -104,7 +122,19 @@ const Header = () => {
                             {
                                 navItems.map(item => (
                                     <li key={item.label}>
-                                        <Link href={item.path} title={item.title}>{item.label}</Link>
+                                        <Link
+                                            className={
+                                                (router.asPath === item.path || (router.asPath === '/' && item.path === '/#home'))
+                                                    ? styles.active
+                                                    : ''
+                                            }
+                                            href={item.path}
+                                            title={item.title}
+                                            scroll={false}
+                                            onClick={() => setShowMenu(false)}
+                                        >
+                                            {item.label}
+                                        </Link>
                                     </li>
                                 ))
                             }
